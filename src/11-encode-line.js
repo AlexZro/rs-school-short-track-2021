de656ef9a2d452f2d80dceef7e0d0619e84ca848
Oleksandr Zrodnikov <alexzrodnikov@gmail.com>
2021-04-12 18:02:07 +0300

    feat: add solution to 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = [];
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) {
+      arr.push(count === 1 ? str[i] : count + str[i]);
+      count = 1;
+    } else {
+      count++;
+    }
+  }
+  return arr.join('');
 }
 
 module.exports = encodeLine;
